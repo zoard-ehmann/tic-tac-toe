f479f47c599abb242c333a56b8d2dfab56dd275e
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-09-21 21:14:34 +0200

    Use map instead of list in player tables

--- player.py
+++ player.py
@@ -2,12 +2,11 @@ class Player:
     def __init__(self, name, player_nr) -> None:
         self.name = name
         self.player_nr = player_nr
-        self.table = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
+        self.table = {
+            "a": [0, 0, 0],
+            "b": [0, 0, 0],
+            "c": [0, 0, 0],
+        }
 
-
-    def set_table(self, table:list):
-        self.table = table
+    def set_field(self, row:str, col:int):
+        self.table[row][col] = 1
