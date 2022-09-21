2392f859342fa1369f26f3fa5a1a8115b8d03e62
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-09-21 21:14:59 +0200

    Sanitizing list calculation in table

--- table.py
+++ table.py
@@ -6,17 +6,13 @@ class Table:
             "c": [" ", " ", " "],
         }
 
-
     def _recognize_player(self, player:int) -> str:
         if player == 1:
             return "X"
         return "O"
 
-
-    def set_table_field(self, row:str, column:int, player:int) -> int:
-        if row in ['a', 'b', 'c'] and column in [1, 2, 3]:
-            col = column - 1
-            
+    def set_table_field(self, row:str, col:int, player:int) -> int:
+        if row in ['a', 'b', 'c'] and col in [0, 1, 2]:
             if self.table_data[row][col] == " ":
                 self.table_data[row][col] = self._recognize_player(player)
                 return 1
@@ -25,16 +21,16 @@ class Table:
             print('Invalid input! Please try again.')
         return 0
 
-
     def print_table(self):
         print(
             f'''
-            -------------
-            | {self.table_data["a"][0]} | {self.table_data["a"][1]} | {self.table_data["a"][2]} |
-            -------------
-            | {self.table_data["b"][0]} | {self.table_data["b"][1]} | {self.table_data["b"][2]} |
-            -------------
-            | {self.table_data["c"][0]} | {self.table_data["c"][1]} | {self.table_data["c"][2]} |
-            -------------
+                1   2   3
+              -------------
+            A | {self.table_data["a"][0]} | {self.table_data["a"][1]} | {self.table_data["a"][2]} |
+              -------------
+            B | {self.table_data["b"][0]} | {self.table_data["b"][1]} | {self.table_data["b"][2]} |
+              -------------
+            C | {self.table_data["c"][0]} | {self.table_data["c"][1]} | {self.table_data["c"][2]} |
+              -------------
             '''
         )
