42990bae7655b03a19e7a7fc016fba3fad776327
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-09-21 17:47:16 +0200

    Implement main gameplay

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-p1_a = [0, 0, 0]
-p1_b = [0, 0, 0]
-p1_c = [0, 0, 0]
+from table import Table
+from player import Player
 
-def check_result(l1:list, l2:list, l3:list):
-    # 4 cases to win:
-    # - if an array has only 1s
-    if 0 not in (l1 or l2 or l3):
-        print('Win')
 
-    # - if all arrays have 1 at the same index
-    for i in range(2):
-        if (l1[i] and l2[i] and l3[i]) == 1:
-            print('Win')
+table = Table()
 
-    # - if the arrays have 1 at n+1 or n-1 index
-    if l2[1] == 1 and (l1[0] == 1 and l3[2]) == 1 or (l1[2] == 1 and l3[0]):
-        print('Win')
+player1 = input('Please enter the name of Player 1: ')
+player2 = input('Please enter the name of Player 2: ')
 
-check_result(l1=[1,1,0],
-             l2=[1,1,0],
-             l3=[0,0,1])
+p1 = Player(player1, 1)
+p2 = Player(player2, 2)
+curr_player = p1
+
+
+def switch_player(player):
+    if player == p1:
+        return p2
+    return p1
+
+
+game_is_on = True
+while game_is_on:
+    player_loop = True
+    while player_loop:
+        print(f'{curr_player.name}\'s turn\n')
+        row = input('Choose row (a, b, c): ').lower()
+        col = int(input('Choose column (1, 2, 3): '))
+        if table.set_table_field(row=row, column=col, player=curr_player.player_nr): player_loop = False
+        table.print_table()
+    curr_player = switch_player(player=curr_player)
