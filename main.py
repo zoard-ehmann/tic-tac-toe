267144a5601fe529af80c181d347636e8e5a7b70
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-09-22 09:03:19 +0200

    Handle draw situation and refactor code

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
+import os
+
 from table import Table
 from player import Player
 from game import Game
 
+# TODO: add single-player mod
 
 def main():
     table = Table()
     game = Game()
 
-    player1 = input('Please enter the name of Player 1: ').capitalize()
-    player2 = input('Please enter the name of Player 2: ').capitalize()
 
-    p1 = Player(player1, 1)
-    p2 = Player(player2, 2)
-    curr_player = p1
+    def clear_console():
+        os.system('clear')
 
 
-    def sanitize_input(inp:str):
-        if len(inp) == 2:
-            row = inp[0].lower()
-            try:
-                col = int(inp[1]) - 1
-                return (row, col)
-            except ValueError:
-                print('Invalid input! Please try again.')
-        return None
+    def ask_user_input() -> tuple:
+        user_prompt = input('Choose a field (row and column, eg.: a1): ')
+
+        if len(user_prompt) == 2:
+            row = user_prompt[0].lower()
+
+            if row.isalpha():
+                try:
+                    col = int(user_prompt[1]) - 1
+                    return (row, col)
+                except ValueError:
+                    pass
+
+        print('Invalid input! Please try again.')
+        return
 
 
     def switch_player(player:Player) -> Player:
@@ -32,28 +38,41 @@ def main():
         return p1
 
 
+    clear_console()
+    player1 = input('Please enter the name of Player 1 (X): ').capitalize()
+    player2 = input('Please enter the name of Player 2 (O): ').capitalize()
+
+    p1 = Player(player1, 1)
+    p2 = Player(player2, 2)
+    curr_player = p1
+
+    table.print_table()
     game_is_on = True
     while game_is_on:
         player_loop = True
         while player_loop:
-            print(f'\n{curr_player.name}\'s turn\n')
-            # TODO: add single-player mod
-            table.print_table()
-            field = input('Choose field (row and column, eg.: a1): ')
-            inp = sanitize_input(field)
+            field = None
+            print(f'\n{curr_player.name}\'s turn ({curr_player})\n')
 
-            if inp:
-                row = inp[0]
-                col = inp[1]
-                
-                if table.set_table_field(row=row, col=col, player=curr_player.player_nr):
-                    curr_player.set_field(row=row, col=col)
-                    player_loop = False
+            while field == None:
+                field = ask_user_input()
+
+            row = field[0]
+            col = field[1]
+            
+            if table.set_table_field(row=row, col=col, player=curr_player.player_nr):
+                curr_player.set_field(row=row, col=col)
+                player_loop = False
 
         if game.check_state(player=curr_player):
             print(f'{curr_player.name} have won!')
             game_is_on = False
+        elif not table.field_available():
+            print('All the fields have been taken. It\'s a draw.')
+            game_is_on = False
         else:
             curr_player = switch_player(player=curr_player)
+
+        table.print_table()
 
 main()
